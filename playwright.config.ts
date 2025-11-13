708168c67caad8775c39fce7f5c6dc4309c2852f
Malay Solanki <59390136+malay45@users.noreply.github.com>
2025-11-13 12:33:49 +0530

    Modify Playwright config for test execution settings
    Updated Playwright configuration to disable fully parallel tests, set worker count to 1 for CI, and added geolocation settings. Also included additional browser configurations for mobile and branded browsers.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,6 @@ import { defineConfig, devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 // import dotenv from 'dotenv';
-// import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
@@ -14,15 +13,22 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['line'], ['allure-playwright'], ['json', { outputFile: 'test-results.json' }]],
+  // reporter: [
+  //   ['list'], // Console reporter
+  //   ['html'], // HTML reporter
+  //   ['json', { outputFile: 'test-results.json' }],
+  //   ['junit', { outputFile: 'results.xml' }], // Add JUnit for CI integration
+  //   ['allure-playwright'],
+  // ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,15 +36,47 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    permissions: ['geolocation'],
+    geolocation: { latitude: -33.8698439, longitude: 151.2082848 },
+    screenshot: 'only-on-failure',
+    headless: false,
+    launchOptions: {
+      slowMo: 500,
+    },
   },
-
+  timeout: 1000 * 60 * 1000,
+  globalTimeout: 1000 * 60 * 1000,
+  expect: { timeout: 1000 * 60 * 1000 },
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'desktop',
+      testMatch: ['**/tests/web/**/*.spec.ts'],
+      use: {
+        viewport: { width: 1280, height: 800 },
+        browserName: 'chromium',
+      },
     },
-
+    {
+      name: 'desktop',
+      testMatch: ['**/tests/api/**/*.spec.ts'],
+      use: {
+        viewport: { width: 1280, height: 800 },
+        browserName: 'chromium',
+      },
+    },
+    {
+      name: 'mobile',
+      testMatch: ['**/tests/mobile/**/*.spec.ts'],
+      use: {
+        ...devices['iPhone 13'],
+        browserName: 'chromium',
+      },
+    },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], },
+    },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -50,25 +88,24 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'google-chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+    {
+      name: 'microsoft-edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'mobile-chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'mobile-safari',
+      use: { ...devices['iPhone 12'] },
+    },
   ],
+  globalSetup: "./setup",
 
   /* Run your local dev server before starting the tests */
   // webServer: {
